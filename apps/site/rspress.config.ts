ccbfe30970826f1a960508c91eedfba2afc04c73
eden lane <edencore@gmail.com>
2025-07-05 16:49:56 +0200

    added analytics

--- apps/site/rspress.config.ts
+++ apps/site/rspress.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
   root: path.join(__dirname, 'docs'),
   title: 'Kona Editor',
   base: '/editor/',
+  head: [
+    '<script defer src="https://umami.kona.to/script.js" data-website-id="428b0290-b2dc-4810-8f84-d49d19d710ff"></script>'
+  ],
   plugins: [
     pluginFontOpenSans()
   ],
